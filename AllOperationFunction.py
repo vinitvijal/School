fdd29c666c7020dbf899a5028c33ab176c9e4147
Vinit Kumar Vijal <vineet.shanker@soesector17rohini.co.in>
2021-09-09 09:17:23 +0530

    Create AllOperationFunction.py

---
+++ AllOperationFunction.py
@@ -0,0 +1,19 @@
+def oper(num1, num2):
+    print("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬")
+    print("Sum of ",num1," and ",num2," : ",num1 + num2)
+    print()
+    print("Multiplication of ",num1," and ",num2," : ",num1 * num2)
+    print()
+    print("Division of ",num1," and ",num2," : ",num1 / num2)
+    print()
+    print("Subtraction of ",num1," and ",num2," : ",num1 - num2)
+    print()
+    print("Modulus of ",num1," and ",num2," : ",num1 % num2)
+    print("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬")
+
+
+
+
+num1 = int(input("Enter Your First Number : "))
+num2 = int(input("Enter the Second Number : "))
+oper(num1,num2)
